a2bb4a73e0133f09a5d09573cc9f90697de0ecd2
ramrachai <ramrachaim@gmail.com>
2023-02-16 21:58:39 +0800

    about us and contact page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,240 @@
 import React from "react";
+import styled from "styled-components";
+import lady from "../assets/images/about/lady.jpg";
+import bg from "../assets/images/about/bg.jpg";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      <h2>About page </h2>
-    </div>
+    <AboutContainer>
+      <section className="about-section">
+        <div className="container">
+          <div className="row clearfix">
+            <div className="content-column ">
+              <div className="inner-column">
+                <div className="sec-title">
+                  <div className="title">About Us</div>
+                  <h2>
+                    We Are The Leader In <br /> The Ecommerce
+                  </h2>
+                </div>
+                <div className="text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries
+                </div>
+                <div className="email">
+                  Request Quote:{" "}
+                  <span className="theme_color">ramrachaim@gmail.com</span>
+                </div>
+                <Link to="/contact" className="theme-btn btn-style-three">
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+
+            <div className="image-column">
+              <div
+                className="inner-column "
+                data-wow-delay="0ms"
+                data-wow-duration="1500ms"
+              >
+                <div className="image">
+                  <img src={lady} alt="" />
+                  <div className="overlay-box">
+                    <div className="year-box">
+                      <span className="number">15</span>Years of <br />{" "}
+                      Ecommerce <br /> Business
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </AboutContainer>
   );
 };
 
 export default About;
+
+const AboutContainer = styled.div`
+  .about-section {
+    position: relative;
+    padding: 60px 0px;
+  }
+
+  .about-section .content-column {
+    position: relative;
+    margin-bottom: 40px;
+    flex: 1;
+  }
+  .row {
+    display: flex;
+    gap: 20px;
+    justify-content: space-between;
+    align-items: center;
+    @media (max-width: 768px) {
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  .about-section .content-column .inner-column {
+    position: relative;
+    padding-top: 50px;
+    padding-right: 100px;
+  }
+
+  .about-section .content-column .text {
+    position: relative;
+    color: #777777;
+    font-size: 15px;
+    line-height: 2em;
+    margin-bottom: 40px;
+  }
+
+  .about-section .content-column .email {
+    position: relative;
+    color: #252525;
+    font-weight: 700;
+    margin-bottom: 50px;
+  }
+
+  .about-section .image-column {
+    position: relative;
+    margin-bottom: 50px;
+    flex: 1;
+  }
+
+  .about-section .image-column .inner-column {
+    position: relative;
+    padding: 40px 40px 0px 0px;
+    margin-left: 50px;
+  }
+
+  .about-section .image-column .inner-column:after {
+    position: absolute;
+    content: "";
+    right: 0px;
+    top: 0px;
+    left: 40px;
+    bottom: 100px;
+    z-index: -1;
+    border: 2px solid #d7a449;
+  }
+
+  .about-section .image-column .inner-column .image {
+    position: relative;
+  }
+
+  .about-section .image-column .inner-column .image:before {
+    position: absolute;
+    content: "";
+    left: -50px;
+    bottom: -50px;
+    width: 299px;
+    height: 299px;
+    background: url("img/pattern-2.png") no-repeat;
+  }
+
+  .about-section .image-column .inner-column .image img {
+    position: relative;
+    width: 100%;
+    display: block;
+  }
+
+  .about-section .image-column .inner-column .image .overlay-box {
+    position: absolute;
+    left: 40px;
+    bottom: 48px;
+  }
+
+  .about-section .image-column .inner-column .image .overlay-box .year-box {
+    position: relative;
+    color: #ebebeb;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 1.4em;
+    padding-left: 125px;
+  }
+
+  .about-section
+    .image-column
+    .inner-column
+    .image
+    .overlay-box
+    .year-box
+    .number {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 110px;
+    height: 110px;
+    color: #d7a449;
+    font-size: 68px;
+    font-weight: 700;
+    line-height: 105px;
+    text-align: center;
+    background-color: #ffffff;
+    border: 1px solid #d7a449;
+  }
+  .about-section .btn-style-three:before {
+    position: absolute;
+    content: "";
+    left: 10px;
+    top: 10px;
+    z-index: -1;
+    right: -10px;
+    bottom: -10px;
+    background: url("https://i.ibb.co/DKn55Qz/pattern-1.jpg") repeat;
+  }
+  .about-section .btn-style-three:hover {
+    color: #ffffff;
+    background: #d7a449;
+  }
+  .about-section .btn-style-three {
+    position: relative;
+    line-height: 24px;
+    color: #252525;
+    font-size: 15px;
+    font-weight: 700;
+    background: none;
+    display: inline-block;
+    padding: 11px 40px;
+    background-color: #ffffff;
+    text-transform: capitalize;
+    border: 2px solid #d7a449;
+    font-family: "Arimo", sans-serif;
+  }
+  .sec-title2 {
+    color: #fff;
+  }
+  .sec-title {
+    position: relative;
+    padding-bottom: 40px;
+  }
+  .sec-title .title {
+    position: relative;
+    color: #d7a449;
+    font-size: 18px;
+    font-weight: 700;
+    padding-right: 50px;
+    margin-bottom: 15px;
+    display: inline-block;
+    text-transform: capitalize;
+  }
+  .sec-title .title:before {
+    position: absolute;
+    content: "";
+    right: 0px;
+    bottom: 7px;
+    width: 40px;
+    height: 1px;
+    background-color: #bbbbbb;
+  }
+`;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,199 @@
 import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact Page </h2>
-    </div>
+    <ContactContainer>
+      <div className="container">
+        <div className="content">
+          <div className="left-side">
+            <div className="address details">
+              <i className="bx bx-map"></i>
+              <div className="topic">Address</div>
+              <div className="text-one">Surkhet, NP12</div>
+              <div className="text-two">Khagrachai, Bangladesh</div>
+            </div>
+            <div className="phone details">
+              <i className="bx bx-phone-call"></i>
+              <div className="topic">Phone</div>
+              <div className="text-one">+123 345 4567</div>
+              <div className="text-two">+987 666 5555</div>
+            </div>
+            <div className="email details">
+              <i className="bx bx-envelope"></i>
+              <div className="topic">Email</div>
+              <div className="text-one">contact@ramrachai.com</div>
+              <div className="text-two">ramrachaim@gmail.com</div>
+            </div>
+          </div>
+          <div className="right-side">
+            <div className="topic-text">Send us a message</div>
+            <p>
+              If you have any work from me or any types of quries related to my
+              tutorial, you can send me message from here. It's my pleasure to
+              help you.
+            </p>
+            <form action="#">
+              <div className="input-box">
+                <input type="text" placeholder="Enter your name" />
+              </div>
+              <div className="input-box">
+                <input type="text" placeholder="Enter your email" />
+              </div>
+              <div className="input-box message-box"></div>
+              <div className="button">
+                <input type="button" value="Send Now" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </ContactContainer>
   );
 };
 
 export default Contact;
+
+const ContactContainer = styled.div`
+  .container {
+    width: 85%;
+    background: #fff;
+    border-radius: 6px;
+    padding: 60px 60px 30px 40px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    @media (max-width: 768px) {
+      padding-top: 40px;
+    }
+  }
+  .container .content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .container .content .left-side {
+    width: 25%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15px;
+    position: relative;
+  }
+  .content .left-side::before {
+    content: "";
+    position: absolute;
+    height: 70%;
+    width: 2px;
+    right: -15px;
+    top: 50%;
+    transform: translateY(-50%);
+    background: #afafb6;
+  }
+  .content .left-side .details {
+    margin: 14px;
+    text-align: center;
+  }
+  .content .left-side .details i {
+    font-size: 30px;
+    color: #3e2093;
+    margin-bottom: 10px;
+  }
+  .content .left-side .details .topic {
+    font-size: 18px;
+    font-weight: 500;
+  }
+  .content .left-side .details .text-one,
+  .content .left-side .details .text-two {
+    font-size: 14px;
+    color: #afafb6;
+  }
+  .container .content .right-side {
+    width: 75%;
+    margin-left: 75px;
+
+    p {
+      margin: 20px 0;
+    }
+  }
+  .content .right-side .topic-text {
+    font-size: 23px;
+    font-weight: 600;
+    color: #3e2093;
+  }
+  .right-side .input-box {
+    height: 50px;
+    width: 100%;
+    margin: 12px 0;
+  }
+  .right-side .input-box input,
+  .right-side .input-box textarea {
+    height: 100%;
+    width: 100%;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    background: #f0f1f8;
+    border-radius: 6px;
+    padding: 0 15px;
+    resize: none;
+  }
+  .right-side .message-box {
+    min-height: 110px;
+  }
+  .right-side .input-box textarea {
+    padding-top: 6px;
+  }
+  .right-side .button {
+    display: inline-block;
+    margin-top: 12px;
+  }
+  .right-side .button input[type="button"] {
+    color: #fff;
+    font-size: 18px;
+    outline: none;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 6px;
+    background: #3e2093;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+  .button input[type="button"]:hover {
+    background: #5029bc;
+  }
+
+  @media (max-width: 950px) {
+    .container {
+      width: 90%;
+      padding: 30px 40px 40px 35px;
+    }
+    .container .content .right-side {
+      width: 75%;
+      margin-left: 55px;
+    }
+  }
+  @media (max-width: 820px) {
+    .container {
+      margin: 40px 0;
+      height: 100%;
+    }
+    .container .content {
+      flex-direction: column-reverse;
+    }
+    .container .content .left-side {
+      width: 100%;
+      flex-direction: row;
+      margin-top: 40px;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+    .container .content .left-side::before {
+      display: none;
+    }
+    .container .content .right-side {
+      width: 100%;
+      margin-left: 0;
+    }
+  }
+`;
